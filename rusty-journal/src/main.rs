01d90d123e547723432549a661369c62efbbadd1
Adekunle Oseni <adekunleoseni47@gmail.com>
2022-02-20 06:12:03 +0100

    feat(rusty-journal): complete the project

--- rusty-journal/src/main.rs
+++ rusty-journal/src/main.rs
@@ -1,7 +1,37 @@
-mod cli;
+use std::path::PathBuf;
 use structopt::StructOpt;
+use anyhow::anyhow;
+mod cli;
+mod task;
 
-fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+use cli::{Action::*, CommandLineArgs};
+use task::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
 }
-    
+
+fn main() -> anyhow::Result<()>  {
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file.
+    let journal_file = journal_file
+    .or_else(find_default_journal_file)
+    .ok_or(anyhow!("Failed to find journal file."))?;
+
+    // Perform the action.
+    match action {
+        Add { text } => task::add_task(journal_file, Task::new(text)),
+        List => task::list_tasks(journal_file),
+        Done { position } => task::complete_task(journal_file, position),
+    }?;
+
+    Ok(())
+}
